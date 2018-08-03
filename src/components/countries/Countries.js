1f0e55d60ef4d663b2af53dd8820d792a2128048
Moreno Andrade <laforetmoreno@gmail.com>
2018-08-02 21:52:57 -0300

    Add select com nome dos paises

--- src/components/countries/Countries.js
+++ src/components/countries/Countries.js
@@ -1,53 +1,51 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Button, Select } from 'antd';
+import { Select } from 'antd';
 import 'antd/lib/button/style/index.css'
 import 'antd/lib/select/style/index.css'
 import 'antd/lib/icon/style/css.js'
 
 import './Countries.scss';
-import { options } from 'sw-toolbox';
 
 // const urlCountrieName = `https://restcountries.eu/rest/v2/name/${name}`;
 const urlBase = 'https://restcountries.eu/rest/v2/all';
 
 const Option = Select.Option;
 
-const renderOptions = () => {
-  const { countriesNames } = this.state;
-}
-
-
 class Countries extends Component {
 
-  state = {
-    countriesNames: [],
-  }
+  constructor(props) {
+    super(props);
+    this.state = {
+      countries: [],
+    }
 
-  componentWillMount() {
     this.handleSearch();
   }
   
   handleSearch = () => {
     axios.get(urlBase)
-    .then(resp => this.setState({countriesNames: resp.data}))
+    .then(resp => this.setState({countries: resp.data}))
+  }
+
+  renderNames = () => {
+    const { countries } = this.state;
+    if(countries) {
+      return (
+        countries.map(countrie => <Option key={countrie.name}>{countrie.name}</Option>)
+      )
+    }
   }
   
   render () {
-    const { countriesNames, names } = this.state;
     return (
       <div className='Countries__content'>
-      <select>
-        {this.getCountriesNames()}
-      </select>
-      <Button 
-        type="primary" 
-        icon="search" 
-        onClick={this.getCountriesNames}
-        >
-        Search
-        </Button>
-        {this.getCountriesNames()}
+          <Select 
+          defaultValue="Choose a country" 
+          style={{ width: 300 }}
+          >
+            {this.renderNames()}
+          </Select>
       </div>
     )
   }
